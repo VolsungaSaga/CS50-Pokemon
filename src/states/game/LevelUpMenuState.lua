e7a960a448f5f8cfdbac44fa6ed02937475f6411
rpierce <rtp4qg@virginia.edu>
2021-11-24 00:43:37 -0500

    Oops, I forgot to initialize this one. I added the LevelUpMenuState and some logic in TakeTurnState to handle it. Also modified Selection.lua. Everything else is unchanged as of this commit.

---
+++ src/states/game/LevelUpMenuState.lua
@@ -0,0 +1,53 @@
+
+
+LevelUpMenuState = Class{__includes = BaseState}
+
+
+function LevelUpMenuState:init(def, onClose)
+    self.pokemon = def.pokemon
+
+    self.HPInc = def.HPInc
+    self.AttackInc = def.AttackInc
+    self.DefenseInc = def.DefenseInc
+    self.SpeedInc = def.SpeedInc
+
+    self.onClose = onClose
+    --Because, at this point, the pokemon will have already leveled up, we need to 
+    -- project backwards into the past in order to provide an accurate chronicle.
+    self.levelupMenu = Menu{
+        x = VIRTUAL_WIDTH - 64,
+        y = VIRTUAL_HEIGHT - 64,
+        width = 64,
+        height = 64,
+        items = {
+            {
+                --Only define onSelect for the first item, because our plain list flag ensures that this is the one called.
+                text = "HP:"..tostring(self.pokemon.HP - self.HPInc).." + "..tostring(self.HPInc).." = "..tostring(self.pokemon.HP),
+                onSelect = function ()
+                    gStateStack:pop()
+                    self.onClose()
+                end
+            },
+            {
+                text = "Attack:"..tostring(self.pokemon.attack - self.AttackInc).." + "..tostring(self.AttackInc).." = "..tostring(self.pokemon.attack)
+            },
+            {
+                text = "Defense"..tostring(self.pokemon.defense - self.DefenseInc).." + "..tostring(self.DefenseInc).." = "..tostring(self.pokemon.defense)
+            },
+            {
+                text = "Speed"..tostring(self.pokemon.speed - self.SpeedInc).." + "..tostring(self.SpeedInc).." = "..tostring(self.pokemon.speed)
+            }
+        },
+        isPlainList = true
+    }
+
+
+end
+
+function LevelUpMenuState:update(dt)
+    
+end
+
+function LevelUpMenuState:render()
+    self.levelupMenu:render()
+end
